a74a486c25925cce7f25d818a96d9d4f70993ae5
Atharva <athrudev@gmail.com>
2025-11-09 21:20:20 +0530

    Added the comment's in the PC program

--- pc.c
+++ pc.c
@@ -4,6 +4,10 @@
 #include <semaphore.h>
 #include <unistd.h>
 
+// producer produces the item and put it in the buffer
+// consumer take that item from the buffer and do it's process
+
+
 #define BUFFER_SIZE 5   // Size of the buffer
 
 int buffer[BUFFER_SIZE];
@@ -19,6 +23,15 @@ void* producer(void* arg) {
     int item;
     for (int i = 1; i <= 10; i++) {
         sem_wait(&empty); // Wait for empty slot
+/*
+sem_wait(&empty); makes the producer wait if the buffer is full. 
+It ensures that the producer only produces when there’s at least one empty slot available in the buffer.
+
+        This is a counting semaphore operation.
+        It decrements the value of the semaphore empty.
+        If empty is greater than 0, it decrements and continues immediately.
+        If empty is 0, it means the buffer is full →the producer thread is blocked (waits) until a consumer signals that a slot has become empty*/
+
         pthread_mutex_lock(&mutex);
 
         // Critical section
@@ -28,6 +41,12 @@ void* producer(void* arg) {
 
         pthread_mutex_unlock(&mutex);
         sem_post(&full); // Signal full slot
+
+        /*
+        sem_post(&full); signals that the producer has produced an item and increased the count of full slots, allowing a consumer to proceed.
+        This operation increments the semaphore full.
+            It means the producer has added a new item → now one more slot is full.
+            If any consumer thread was waiting on sem_wait(&full);, it gets unblocked (woken up) to consume an item.*/
 
         sleep(4); // Simulate time gap
     }
@@ -39,15 +58,21 @@ void* consumer(void* arg) {
     int item;
     for (int i = 0; i < 10; i++) {
         sem_wait(&full); // Wait for available item
+        /*
+        sem_wait(&full) makes the consumer wait if the buffer is empty. It ensures the consumer only consumes when there is at least one filled slot.
         pthread_mutex_lock(&mutex);
+        */
 
         // Critical section
         item = buffer[out];
         printf("Consumer consumed: %d\n", item);
         out = (out + 1) % BUFFER_SIZE;
-
+        /*Updates the out index using the circular buffer formula:
+        (out + 1) % BUFFER_SIZE ensures that once the end of the buffer is reached, it wraps around to index 0.*/
         pthread_mutex_unlock(&mutex);
         sem_post(&empty); // Signal empty slot
+        /*Increments the empty semaphore count.
+        Signals that there is now one more empty slot available in the buffer for the producer to use*/
 
         sleep(2); // Simulate time gap
     }
